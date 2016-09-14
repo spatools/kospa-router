ffff78d0044f615da8901c08c23dc93543ab6ee1
Maxime LUCE <maxime@touchify.co>
2016-09-14 09:20:59 +0200

    Initial release

---
+++ index.d.ts
@@ -0,0 +1,70 @@
+import * as ko from "knockout";
+import * as composer from "@kospa/base/composer";
+import * as activator from "@kospa/base/activator";
+export interface Options {
+    mode?: string;
+    root?: string;
+    onError?: (err: any) => any;
+}
+export interface Route {
+    matcher: RegExp;
+    handlers: RouteHandler[];
+}
+export interface Routes {
+    [key: string]: Route;
+    none: Route;
+}
+export declare type RouteHandler = (...args: any[]) => any;
+export declare class BaseRouter {
+    private static skip;
+    private _current;
+    private timeout;
+    private _onError;
+    private rootRegExp;
+    private routes;
+    mode: string;
+    root: string;
+    constructor(options?: Options);
+    use(...handlers: RouteHandler[]): BaseRouter;
+    use(path: string | RegExp, ...handlers: RouteHandler[]): BaseRouter;
+    unuse(...handlers: RouteHandler[]): BaseRouter;
+    unuse(path: string | RegExp, ...handlers: RouteHandler[]): BaseRouter;
+    add(path: string | RegExp, ...handlers: RouteHandler[]): BaseRouter;
+    remove(path: string | RegExp, ...handlers: RouteHandler[]): BaseRouter;
+    none(...handlers: RouteHandler[]): BaseRouter;
+    start(): BaseRouter;
+    stop(): BaseRouter;
+    clear(): BaseRouter;
+    navigate(): BaseRouter;
+    navigate(path: string): BaseRouter;
+    replace(): BaseRouter;
+    replace(path: string): BaseRouter;
+    replace(path: string, skipHandling: boolean): BaseRouter;
+    handle(): Promise<BaseRouter>;
+    handle(fragment: string): Promise<BaseRouter>;
+    getFragment(): string;
+    protected onError(err: any): any;
+}
+export interface ViewModelRoute extends composer.CompositionOptions {
+    path: string | RegExp;
+    href?: string | ko.Observable<string>;
+    title?: string | ko.Observable<string>;
+    visible?: boolean | ko.Observable<boolean>;
+    handler?: RouteHandler;
+}
+export declare class Router extends BaseRouter {
+    private routeHandlers;
+    currentRoute: ko.Observable<ViewModelRoute>;
+    currentViewModel: activator.ActivateObservable<activator.ViewModel>;
+    isNavigating: ko.Observable<boolean>;
+    navigation: ko.PureComputed<ViewModelRoute[]>;
+    constructor(options?: Options);
+    route(config: ViewModelRoute): Router;
+    deroute(config: ViewModelRoute): Router;
+    notFound(config: ViewModelRoute): Router;
+    child(path: string, childRouter?: Router): Router;
+    clear(): Router;
+    handle(fragment?: string): Promise<Router>;
+}
+declare const rootRouter: Router;
+export default rootRouter;
---
+++ index.js
@@ -0,0 +1,361 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+(function (factory) {
+    if (typeof module === 'object' && typeof module.exports === 'object') {
+        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === 'function' && define.amd) {
+        define(["require", "exports", "knockout", "@kospa/base/composer", "@kospa/base/system", "@kospa/base/activator"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    var ko = require("knockout");
+    var composer = require("@kospa/base/composer");
+    var system = require("@kospa/base/system");
+    var activator = require("@kospa/base/activator");
+    //#region BaseRouter Class
+    var hasHistory = !!history.pushState, push = Array.prototype.push, slice = Array.prototype.slice;
+    var BaseRouter = (function () {
+        function BaseRouter(options) {
+            this.routes = {
+                none: { matcher: null, handlers: [baseNotFound] }
+            };
+            this.mode = hasHistory ? "history" : "hash";
+            this.root = "";
+            if (options) {
+                if (options.mode) {
+                    this.mode = options.mode === "history" && hasHistory ? "history" : "hash";
+                }
+                var root = normalizeRoute(options.root);
+                if (root) {
+                    this.root = root + "/";
+                    this.rootRegExp = new RegExp("^" + this.root + "?");
+                }
+                if (options.onError) {
+                    this._onError = options.onError;
+                }
+            }
+        }
+        BaseRouter.prototype.use = function () {
+            var config = createRouteConfig(arguments), routeId = config.matcher.toString(), route = this.routes[routeId];
+            if (route) {
+                push.apply(route.handlers, config.handlers);
+            }
+            else {
+                this.routes[routeId] = config;
+            }
+            return this;
+        };
+        BaseRouter.prototype.unuse = function () {
+            var config = createRouteConfig(arguments), routeId = config.matcher.toString(), route = this.routes[routeId];
+            if (route) {
+                var handler = void 0, index = void 0;
+                while (handler = config.handlers.pop()) {
+                    index = route.handlers.indexOf(handler);
+                    if (index !== -1) {
+                        route.handlers.splice(index, 1);
+                    }
+                }
+                if (route.handlers.length === 0) {
+                    delete this.routes[routeId];
+                }
+            }
+            return this;
+        };
+        BaseRouter.prototype.add = function (path) {
+            var handlers = [];
+            for (var _i = 1; _i < arguments.length; _i++) {
+                handlers[_i - 1] = arguments[_i];
+            }
+            var routeRegExp = typeof path === "string" ? createRouteRegExp(path) : path;
+            return this.use.apply(this, [routeRegExp].concat(handlers));
+        };
+        BaseRouter.prototype.remove = function (path) {
+            var handlers = [];
+            for (var _i = 1; _i < arguments.length; _i++) {
+                handlers[_i - 1] = arguments[_i];
+            }
+            var routeRegExp = typeof path === "string" ? createRouteRegExp(path) : path;
+            return this.unuse.apply(this, [routeRegExp].concat(handlers));
+        };
+        BaseRouter.prototype.none = function () {
+            var handlers = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                handlers[_i - 0] = arguments[_i];
+            }
+            var route = this.routes.none;
+            if (route.handlers[0] === baseNotFound) {
+                route.handlers = handlers;
+            }
+            else {
+                push.apply(route.handlers, handlers);
+            }
+            return this;
+        };
+        BaseRouter.prototype.start = function () {
+            var current = null, self = this;
+            this.stop();
+            delay();
+            function delay() {
+                self.timeout = setTimeout(handle, 50);
+            }
+            function handle() {
+                if (BaseRouter.skip) {
+                    BaseRouter.skip = false;
+                    current = self._current;
+                    return delay();
+                }
+                var f = self.getFragment();
+                if (current !== f) {
+                    current = f;
+                    self.handle(f).then(delay, delay);
+                }
+                else {
+                    delay();
+                }
+            }
+            return this;
+        };
+        BaseRouter.prototype.stop = function () {
+            clearInterval(this.timeout);
+            return this;
+        };
+        BaseRouter.prototype.clear = function () {
+            this.stop();
+            this.routes = {
+                none: { matcher: null, handlers: [baseNotFound] }
+            };
+            this.mode = "hash";
+            this.root = "";
+            return this;
+        };
+        BaseRouter.prototype.navigate = function (path) {
+            path = this.root + normalizeRoute(path);
+            BaseRouter.skip = false;
+            if (hasHistory) {
+                history.pushState(null, null, this.mode === "history" ? path : "#" + path);
+            }
+            else {
+                location.hash = "#" + path;
+            }
+            return this;
+        };
+        BaseRouter.prototype.replace = function (path, skipHandling) {
+            path = this.root + normalizeRoute(path);
+            BaseRouter.skip = skipHandling || false;
+            if (hasHistory) {
+                history.replaceState(null, null, this.mode === "history" ? path : "#" + path);
+            }
+            else {
+                location.hash = "#" + path;
+            }
+            return this;
+        };
+        BaseRouter.prototype.handle = function (fragment) {
+            var _this = this;
+            if (fragment === void 0) { fragment = this.getFragment(); }
+            if (fragment === null) {
+                return Promise.resolve(this);
+            }
+            var handlers = [], routeId, route, match;
+            for (routeId in this.routes) {
+                if (match = fragment.match(this.routes[routeId].matcher)) {
+                    handlers.push(executeHandlers.bind(this, this.routes[routeId].handlers, slice.call(match, 1)));
+                }
+            }
+            if (handlers.length === 0) {
+                handlers = this.routes.none.handlers;
+            }
+            return executeHandlers(handlers)
+                .catch(this.onError.bind(this))
+                .then(function () {
+                _this._current = fragment;
+                return _this;
+            });
+        };
+        BaseRouter.prototype.getFragment = function () {
+            var fragment = this.mode === "history" ? location.pathname : location.hash;
+            fragment = normalizeRoute(fragment);
+            if (this.root === "") {
+                return fragment;
+            }
+            if (this.rootRegExp.test(fragment)) {
+                return fragment.replace(this.rootRegExp, "");
+            }
+            return null;
+        };
+        BaseRouter.prototype.onError = function (err) {
+            console.log("err", err);
+            var onErr = this._onError || baseOnError;
+            return onErr.call(this, err);
+        };
+        BaseRouter.skip = false;
+        return BaseRouter;
+    }());
+    exports.BaseRouter = BaseRouter;
+    //#region Private Methods
+    function createRouteConfig(args) {
+        var i = 0, arg = args[0], routeRegExp;
+        if (typeof arg === "function") {
+            routeRegExp = /(.*)/;
+        }
+        else {
+            routeRegExp = typeof arg === "string" ? createRouteRegExp(arg + "(*)") : arg;
+            i = 1;
+        }
+        return {
+            matcher: routeRegExp,
+            handlers: slice.call(args, i)
+        };
+    }
+    function normalizeRoute(path) {
+        return String(path || "")
+            .replace(/^#/, "")
+            .replace(/\/$/, "")
+            .replace(/^\//, "");
+    }
+    function createRouteRegExp(route) {
+        route = normalizeRoute(route);
+        route = route.replace("*", ".*");
+        route = route.replace(/:[^\/]+/g, "([^\\/]+)");
+        return new RegExp("^" + route + "$");
+    }
+    function executeHandlers(handlers, args) {
+        var p = Promise.resolve(), i = 0, len = handlers.length;
+        for (; i < len; i++) {
+            p = p.then(executeHandler.bind(null, handlers[i], args));
+        }
+        return p;
+    }
+    function executeHandler(handler, args) {
+        return handler.apply(null, args);
+    }
+    function baseNotFound() {
+        throw new Error("Not Found");
+    }
+    function baseOnError(err) {
+        system.error("router>", err);
+        if (this._current) {
+            this.replace(this._current, true);
+        }
+        throw err;
+    }
+    var Router = (function (_super) {
+        __extends(Router, _super);
+        function Router(options) {
+            var _this = this;
+            _super.call(this, options);
+            this.routeHandlers = {};
+            this.currentRoute = ko.observable();
+            this.currentViewModel = activator.createActivateObservable();
+            this.isNavigating = ko.observable(false);
+            this.navigation = ko.pureComputed(function () {
+                return Object.keys(_this.routeHandlers)
+                    .map(function (key) { return _this.routeHandlers[key]; })
+                    .filter(function (config) { return ko.unwrap(config.visible); });
+            });
+        }
+        Router.prototype.route = function (config) {
+            var handlerId = config.path.toString(), handler = createRouteHandler(this, config);
+            config.handler = handler;
+            if (!config.href && typeof config.path === "string") {
+                config.href = (this.mode === "hash" ? "#" : "") + this.root + config.path;
+            }
+            this.routeHandlers[handlerId] = config;
+            this.add(config.path, handler);
+            return this;
+        };
+        Router.prototype.deroute = function (config) {
+            var handlerId = config.path.toString(), innerConfig = this.routeHandlers[handlerId];
+            if (innerConfig) {
+                this.remove(innerConfig.path, config.handler);
+                delete this.routeHandlers[handlerId];
+            }
+            return this;
+        };
+        Router.prototype.notFound = function (config) {
+            this.none(createRouteHandler(this, config));
+            return this;
+        };
+        Router.prototype.child = function (path, childRouter) {
+            if (!childRouter) {
+                childRouter = new Router({ mode: this.mode, root: path, onError: this.onError.bind(this) });
+            }
+            this.use(new RegExp("^" + path + "(?:/(.*))?$"), childRouter.handle.bind(childRouter));
+            return childRouter;
+        };
+        Router.prototype.clear = function () {
+            _super.prototype.clear.call(this);
+            this.routeHandlers = {};
+            this.currentRoute(null);
+            this.currentViewModel(null);
+            return this;
+        };
+        Router.prototype.handle = function (fragment) {
+            var _this = this;
+            if (fragment === null) {
+                return Promise.resolve(this);
+            }
+            this.isNavigating(true);
+            return _super.prototype.handle.call(this, fragment).then(function () {
+                _this.isNavigating(false);
+                return _this;
+            }, function (err) {
+                _this.isNavigating(false);
+                throw err;
+            });
+        };
+        return Router;
+    }(BaseRouter));
+    exports.Router = Router;
+    var rootRouter = new Router();
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = rootRouter;
+    function createRouteHandler(self, route) {
+        return function () {
+            var oldRoute = self.currentRoute();
+            self.currentRoute(route);
+            self.currentViewModel.args = slice.call(arguments);
+            self.currentViewModel(route.viewmodel);
+            return self.currentViewModel.then(function (vm) {
+                document.title = vm && vm.title ? ko.unwrap(vm.title) : ko.unwrap(route.title);
+            }, function (err) {
+                self.currentRoute(oldRoute);
+                throw err;
+            });
+        };
+    }
+    //#endregion
+    //#region Router Handlers
+    ko.bindingHandlers["router"] = {
+        init: function (element, valueAccessor) {
+            var val = valueAccessor(), router;
+            if (val instanceof Router) {
+                router = val;
+            }
+            else {
+                val = typeof val === "object" ? val.router : val;
+                router = val || rootRouter;
+            }
+            var container = document.createElement("div");
+            ko.virtualElements.setDomNodeChildren(element, [container]);
+            ko.computed({
+                disposeWhenNodeIsRemoved: element,
+                read: function () {
+                    var config = router.currentRoute.peek(), vm = router.currentViewModel();
+                    if (!config || !vm) {
+                        return;
+                    }
+                    composer.compose(container, system.extend({}, config, { viewmodel: vm }))
+                        .catch(system.error);
+                }
+            });
+        }
+    };
+    ko.virtualElements.allowedBindings["router"] = true;
+    ko.components.register("kospa-router", { template: "<!--ko router: $data--><!--/ko-->" });
+});
+//#endregion
